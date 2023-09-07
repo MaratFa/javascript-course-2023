b9f6328e6b4a6a4084bbf5f37a833984243f2ce0
Marat <evolution99@rambler.ru>
2023-09-08 00:10:29 +0300

    Lesson 3 - Arithmetic expressions

---
+++ 3. Lesson 3 -  Arithmetic expressions/index.js
@@ -0,0 +1,33 @@
+/*
+    arithmetic expression is a combination of...
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evalueted to a value
+
+    example: y = x + 5;
+*/
+
+// let students = 20;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// let extraStudents = students % 2;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students = 2;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division
+    4. addition & subtraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result);

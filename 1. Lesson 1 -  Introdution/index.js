56622ec19272d6e30a283a4b0078d39f2918be8a
Marat <evolution99@rambler.ru>
2023-09-07 17:35:46 +0300

    first commit

---
+++ 1. Lesson 1 -  Introdution/index.js
@@ -0,0 +1,14 @@
+console.log("I like pizza");
+console.log("It's really good");
+
+window.alert("I really love pizza!");
+
+// This a a comment
+
+/*
+    This
+    is 
+    a 
+    multiline
+    comment
+*/

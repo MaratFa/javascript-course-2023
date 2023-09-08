830a5e2cbd56af7e88e6ce1e3fd8d7c0f082ba05
Marat <evolution99@rambler.ru>
2023-09-08 22:48:19 +0300

    Lesson 4 - User input

--- 4. Lesson 4 -  User input/index.js
+++ 4. Lesson 4 -  User input/index.js
@@ -2,7 +2,16 @@
 
 // EASY WAY with a window prompt
 
-let username = window.prompt("What's your name?")
-console.log(username);
+// let username = window.prompt("What's your name?")
+// console.log(username);
 
 // DIFFICULT WAY HTML textbox
+
+let username;
+
+document.getElementById("myButton").onclick = function(){
+    
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+}

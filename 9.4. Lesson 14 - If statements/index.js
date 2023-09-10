951e6da2d5f7ba8c0502950d25ad53cad5045622
Marat <evolution99@rambler.ru>
2023-09-11 00:12:53 +0300

    Lesson 14 - If statements

---
+++ 9.4. Lesson 14 - If statements/index.js
@@ -0,0 +1,25 @@
+// if statement = a basic form of decision making
+//                if a condition is true, then do somethig
+//                if not, then don't do it!
+
+/*
+let age = -3;
+
+if (age >= 65) {
+    console.log("You are a senior citizen!");    
+} else if (age >= 18) {
+    console.log("You are an adult!");
+} else if (age < 0) {
+  console.log("YOU HAVEN'T BORN YET!");
+} else {
+  console.log("You are a child!");
+}
+*/
+
+let online = 0;
+
+if (online) {
+  console.log("You are online!");
+} else {
+  console.log("You are offline!");
+}

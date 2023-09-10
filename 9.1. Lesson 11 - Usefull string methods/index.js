2073848f1b4c6a2182caaa138e5854a091f9c20c
Marat <evolution99@rambler.ru>
2023-09-10 04:33:32 +0300

    Lesson 11 - Usefull string methods

---
+++ 9.1. Lesson 11 - Usefull string methods/index.js
@@ -0,0 +1,13 @@
+// Usefull string properties and methods
+
+let userName = "  John Johnson    ";
+let phoneNumber = "123-456-7890";
+
+// console.log(userName.length);
+// console.log(userName.charAt(2));
+// console.log(userName.indexOf("s"));
+// console.log(userName.lastIndexOf("o"));
+// console.log(userName.trim());
+// console.log((userName = userName.toUpperCase()));
+// console.log((userName = userName.toLowerCase()));
+console.log(phoneNumber.replaceAll("-", ""));

7d06398b35bbc25915aa5af82a1147d0d6bd19d6
Marat <evolution99@rambler.ru>
2023-09-14 10:08:43 +0300

    Lesson 29 - Format currency

--- 9.9.9. Lesson 29 - Format currency/index.js
+++ 9.9.9. Lesson 29 - Format currency/index.js
@@ -6,15 +6,17 @@
 // 'locale' = specify that language (undefined = default set in browser)
 // 'options' = object with formatting options
 
-let myNum = 123456.789;
+let myNum = 100;
 
 // myNum = myNum.toLocaleString("en-US"); // US English
 // myNum = myNum.toLocaleString("hi-IN"); // Hindi
 // myNum = myNum.toLocaleString("de-DE"); // standard German
 
-myNum = myNum.toLocaleString("en-US", {style: })
+// myNum = myNum.toLocaleString("en-US", { style: "currency", currency: "USD" });
+// myNum = myNum.toLocaleString("hi-IN", { style: "currency", currency: "INR" });
+// myNum = myNum.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
 
-
-
+// myNum = myNum.toLocaleString(undefined, { style: "percent" });
+myNum = myNum.toLocaleString(undefined, { style: "unit", unit: "kilometer" });
 
 console.log(myNum);

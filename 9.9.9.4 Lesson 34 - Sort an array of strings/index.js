b1883a9e59efd1ff84fe385edfc585f6653370e1
Marat <evolution99@rambler.ru>
2023-09-15 11:27:23 +0300

    Lesson 34 - Sort an array of strings

---
+++ 9.9.9.4 Lesson 34 - Sort an array of strings/index.js
@@ -0,0 +1,10 @@
+let fruits = ["banana", "apple", "orange", "mango"];
+
+// fruits = fruits.sort();
+fruits = fruits.sort().reverse();
+
+console.log(fruits);
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}

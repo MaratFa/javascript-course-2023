b51c59410a532490bd73a564cac651bf456b833e
Marat <evolution99@rambler.ru>
2023-09-20 00:16:05 +0300

    Lesson 69 - 71

---
+++ 9.9.9.9.9.9.9.1 Lesson 71 -  DOM intro/index.js
@@ -0,0 +1,11 @@
+// DOM = Document Object Model (API)
+//       An interface for changing the content of a page
+
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.URL);
+// document.title = "New title"
+// document.location = "http://www.google.com";
+
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "Hello!"

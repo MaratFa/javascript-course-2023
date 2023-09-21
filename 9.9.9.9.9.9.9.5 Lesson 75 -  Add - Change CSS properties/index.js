8a6d277ab00929c49f5b47426898246b04007387
Marat <evolution99@rambler.ru>
2023-09-21 07:39:45 +0300

    Lesson 75 - Add-Change CSS properties

---
+++ 9.9.9.9.9.9.9.5 Lesson 75 -  Add - Change CSS properties/index.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("myTitle");
+
+title.style.backgroundColor = "hsl(180, 100%, 68%)";
+title.style.color = "rgb(50, 100, 250)";
+title.style.fontFamily = "Arial";
+title.style.textAlign = "center";
+title.style.border = "5px solid green";
+title.style.display = "block";

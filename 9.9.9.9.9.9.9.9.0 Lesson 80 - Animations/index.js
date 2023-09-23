894d6c0faf6bf8aaa289a0a80c3667116f6bc988
Marat <evolution99@rambler.ru>
2023-09-23 07:52:01 +0300

    Lesson 80 - Animations

---
+++ 9.9.9.9.9.9.9.9.0 Lesson 80 - Animations/index.js
@@ -0,0 +1,22 @@
+const myButton = document.getElementById("myButton");
+const myAnimation = document.getElementById("myDiv");
+
+myButton.addEventListener("click", begin);
+
+function begin() {
+  let timerId = null;
+  let scaleX = 1;
+  let scaleY = 1;
+
+  timerId = setInterval(frame, 1);
+
+  function frame() {
+    if (scaleX <= 0.1 || scaleY <= 0.1) {
+      clearInterval(timerId);
+    } else {
+      scaleX -= 0.01;
+      scaleY -= 0.02;
+      myAnimation.style.transform = "scale(" + scaleX + "," + scaleY + ")";
+    }
+  }
+}

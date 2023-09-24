fb7fb79d1d137878e2cf312b7bb04b3605725861
Marat <evolution99@rambler.ru>
2023-09-24 09:25:26 +0300

    Lesson 83 - Cookies

---
+++ 9.9.9.9.9.9.9.9.3 Lesson 83 - Cookies/index.js
@@ -0,0 +1,41 @@
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
+
+submitBtn.addEventListener("click", () => {
+  setCookie("firstName", firstText.value, 365);
+  setCookie("lastName", lastText.value, 365);
+});
+
+cookieBtn.addEventListener("click", () => {
+  firstText.value = getCookie("firstName");
+  lastText.value = getCookie("lastName");
+});
+
+deleteCookie("firstName");
+deleteCookie("lastName");
+
+function setCookie(name, value, daysToLive) {
+  const date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function deleteCookie(name) {
+  setCookie(name, null, null);
+}
+
+function getCookie(name) {
+  const cookieDecoded = decodeURIComponent(document.cookie);
+  const cookieArray = cookieDecoded.split("; ");
+  let result = null;
+
+  cookieArray.forEach((element) => {
+    if (element.indexOf(name) == 0) {
+      result = element.substring(name.length + 1);
+    }
+  });
+  return result;
+}

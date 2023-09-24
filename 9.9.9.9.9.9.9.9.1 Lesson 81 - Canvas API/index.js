04c6ac682ee16ba231ea840e0b092b0c8512fe12
Marat <evolution99@rambler.ru>
2023-09-24 09:24:33 +0300

    Lesson 81 - Canvas API

---
+++ 9.9.9.9.9.9.9.9.1 Lesson 81 - Canvas API/index.js
@@ -0,0 +1,73 @@
+// Canvas API = a means for drawing graphics
+//              used for animations, games, data visualization
+
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+/*
+// DRAW LINES
+context.strokeStyle = "purple";
+context.lineWidth = 10;
+context.beginPath();
+context.moveTo(0, 0);
+context.lineTo(250, 250);
+context.lineTo(250, 500);
+context.moveTo(500, 0);
+context.lineTo(250, 250);
+context.stroke();
+*/
+
+/*
+// DRAW TRIANGLE
+context.strokeStyle = "grey";
+context.fillStyle = "yellow";
+context.lineWidth = 10;
+context.beginPath();
+context.moveTo(250, 0);
+context.lineTo(0, 250);
+context.lineTo(500, 250);
+context.lineTo(250, 0);
+context.stroke();
+context.fill();
+*/
+
+/*
+// DRAW RECTANGLE
+context.fillStyle = "black";
+context.fillRect(0, 0, 250, 250);
+context.strokeStyle = "black";
+context.strokeRect(0, 0, 250, 250);
+
+context.fillStyle = "red";
+context.fillRect(0, 250, 250, 250);
+context.strokeStyle = "black";
+context.strokeRect(0, 250, 250, 250);
+
+context.fillStyle = "green";
+context.fillRect(250, 250, 250, 250);
+context.strokeStyle = "black";
+context.strokeRect(250, 250, 250, 250);
+
+context.fillStyle = "blue";
+context.fillRect(250, 0, 250, 250);
+context.strokeStyle = "black";
+context.strokeRect(250, 250, 250, 250);
+*/
+
+/*
+// DRAW CIRCLE
+// (x, y, r, sAngle, eAngle, counterclockwise)
+context.fillStyle = "lightblue";
+context.strokeStyle = "darkblue"
+context.beginPath();
+context.lineWidth = 10;
+context.arc(250, 250, 200, 0, 2 * Math.PI, true);
+context.stroke();
+context.fill();
+*/
+
+// DRAW TEXT
+context.font = "50px Courier New";
+context.fillStyle = "green";
+context.textAlign = "center";
+context.fillText("YOU WIN!", canvas.width / 2, canvas.height / 2);

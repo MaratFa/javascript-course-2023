20d7032a4c4ebc18f87f300e4be4a2ae70d60ce4
Marat <evolution99@rambler.ru>
2023-09-24 09:24:56 +0300

    Lesson 82 - Window

---
+++ 9.9.9.9.9.9.9.9.2 Lesson 82 - Window /index.js
@@ -0,0 +1,26 @@
+// window = interface used to talk the web browser
+//          the DOM is a property of the window
+
+const myButton = document.querySelector("#myButton");
+
+console.dir(window);
+// console.log(window.outerWidth);
+// console.log(window.outerHeight);
+// console.log(window.scrollX);
+// console.log(window.scrollY);
+
+// console.log(window.location.href);
+// window.location.href = "https://google.com";
+// console.log(window.location.hostname);
+// console.log(window.location.pathname)
+
+// myButton.addEventListener("click", () => window.open("https://google.com"));
+// myButton.addEventListener("click", () => window.close());
+// myButton.addEventListener("click", () => window.print());
+
+// window.alert("Hello!")
+// window.confirm("Press OK to continue!");
+let age = window.prompt("Enter your age");
+if (age < 18) {
+  window.alert("You must be 18+ to visit this site!");
+}
